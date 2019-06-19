55419d1c116cd539d8946c14ddfdf5eb86c87a3f
Sohil Rupani <sohil.rupani0@walmartlabs.com>
2019-06-19 13:37:35 -0700

    Init Project

---
+++ CarthageTesting/CarthageTesting/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CarthageTesting
+//
+//  Created by Sohil Rupani on 6/19/19.
+//  Copyright © 2019 Sohil Rupani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ CarthageTesting/CarthageTestingTests/CarthageTestingTests.m
@@ -0,0 +1,37 @@
+//
+//  CarthageTestingTests.m
+//  CarthageTestingTests
+//
+//  Created by Sohil Rupani on 6/19/19.
+//  Copyright © 2019 Sohil Rupani. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CarthageTestingTests : XCTestCase
+
+@end
+
+@implementation CarthageTestingTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
