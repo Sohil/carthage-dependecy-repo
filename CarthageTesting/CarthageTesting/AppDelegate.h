d5388ab8a3cb55112face1ee37a061ac85e1d5cc
Sohil Rupani <sohil.rupani0@walmartlabs.com>
2019-06-19 13:37:35 -0700

    Init Project

---
+++ CarthageTesting/CarthageTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarthageTesting
+//
+//  Created by Sohil Rupani on 6/19/19.
+//  Copyright © 2019 Sohil Rupani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CarthageTesting/CarthageTesting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarthageTesting
+//
+//  Created by Sohil Rupani on 6/19/19.
+//  Copyright © 2019 Sohil Rupani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
